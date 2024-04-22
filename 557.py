57ab06c81cd8094d0d0cde49534697827bf3067e
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-04-22 18:35:18 +0500

    Solve 557

---
+++ 557.py
@@ -0,0 +1,31 @@
+from typing import List
+from math import floor
+import unittest
+        
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        left = 0
+        output = []
+        right = 0
+        for right in range(len(s)):
+            if s[right] == ' ':
+                output.append(s[left:right][::-1])
+                left = right + 1
+
+
+        output.append(s[left:right+1][::-1])
+                
+        print(" ".join(output))
+        return " ".join(output)
+
+
+class TestSolution(unittest.TestCase):
+    def runTest(self):
+        solution = Solution()
+        self.assertEqual(solution.reverseWords("Let's take LeetCode contest"), "s'teL ekat edoCteeL tsetnoc")
+        self.assertEqual(solution.reverseWords("Mr Ding"), "rM gniD")
+
+
+
+unittest.main()
+        
