9a8ebf908703c57ed9a88c21282f05c4f636c19f
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-04-23 17:50:07 +0500

    Solve 283

---
+++ 283.py
@@ -0,0 +1,36 @@
+from typing import List
+from math import floor
+import unittest
+        
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        right = 0
+        while left < len(nums):
+            if nums[left] != 0:
+                nums[right] = nums[left]
+                right += 1
+            left += 1
+
+        while right < len(nums):
+            nums[right] = 0
+            right += 1
+                
+
+
+
+        
+class TestSolution(unittest.TestCase):
+    def runTest(self):
+        solution = Solution()
+        nums1 = [0,1,0,3,12]
+        nums2 = [0]
+        solution.moveZeroes(nums1)
+        self.assertEqual(nums1, [1,3,12,0,0])
+        solution.moveZeroes(nums2)
+        self.assertEqual(nums2, [0])
+
+
+
+unittest.main()
+        
