cb288743647ddac1468af3a6110bdcc9eb0910fb
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-04-25 18:05:47 +0500

    Solve 724

---
+++ 724.py
@@ -0,0 +1,24 @@
+from typing import List
+import unittest
+        
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        sums = [nums[0]]
+        for left in range(1, len(nums)):
+            sums.append(sums[-1] + nums[left])
+
+        for left in range(len(nums)):
+            if sums[left] == sums[-1] - sums[left] + nums[left]:
+                return left
+
+        return -1
+         
+class TestSolution(unittest.TestCase):
+    def runTest(self):
+        solution = Solution()
+        self.assertEqual(solution.pivotIndex([1,7,3,6,5,6]), 3)
+        self.assertEqual(solution.pivotIndex([1,2,3]), -1)
+        self.assertEqual(solution.pivotIndex([2,1,-1]), 0)
+
+unittest.main()
+        
