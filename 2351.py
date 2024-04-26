f8d9e29d320b5c3f85d7b65a4902f8dce9a15c60
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-04-26 14:22:17 +0500

    Solve 2351

---
+++ 2351.py
@@ -0,0 +1,24 @@
+from typing import List
+import unittest
+
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        visited = set()
+        for i in range(len(s)):
+            if s[i] in visited:
+                return s[i]
+
+            visited.add(s[i])
+        return s
+
+
+class TestSolution(unittest.TestCase):
+    def runTest(self):
+        solution = Solution()
+        self.assertEqual(solution.repeatedCharacter("abccbaacz"), "c")
+        self.assertEqual(solution.repeatedCharacter("abcdd"), "d")
+
+
+
+unittest.main()
