b64ae8219cc8ed370fc1b292527904a13664b660
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-11-13 16:08:07 +0200

    Add prev pointer to linked list

--- linked_list.py
+++ linked_list.py
@@ -1,7 +1,8 @@
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=0, next=None, prev=None):
         self.val = val
         self.next = next
+        self.prev = prev
 
     def to_list(self):
         result = []
